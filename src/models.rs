7c45ba1de42ed6dab8c8bb93ff17d119fd5a3d56
traximullin <traximullin@bk.ru>
2024-01-15 21:17:51 +0300

    Добавлен сервис

--- src/models.rs
+++ src/models.rs
@@ -1,11 +1,26 @@
+use crate::schema::posts;
 use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Selectable)]
-#[diesel(table_name = crate::schema::posts)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
+#[derive(Queryable, Selectable, Serialize, Deserialize)]
 pub struct Post {
     pub id: i32,
     pub title: String,
     pub body: String,
     pub published: bool,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = posts)]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
+    pub published: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NewPostPayload {
+    pub title: String,
+    pub body: String,
+    pub published: Option<bool>,
 }
